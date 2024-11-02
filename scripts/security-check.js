a1a27b51deb46e3ee7eeb498a4cffe1e3f955787
dicknosesquad <dicknosesquad@gmail.com>
2024-11-01 19:34:47 -0700

    Delete scripts/security-check.js

--- scripts/security-check.js
+++
@@ -1,7 +0,0 @@
-function runCSPTest() {
-    const cspResults = document.getElementById("csp-results");
-
-    // Simulate static check for CSP
-    cspResults.innerHTML = `<p>Testing CSP for configurations...</p>`;
-    // Further analysis logic can be added here
-}
